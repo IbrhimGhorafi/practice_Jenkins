0d8329cda498f8994d0b7d800ae10837e29a6dba
Ibrahim Ghorafi <ighorafi@smb.sqli.com>
2024-03-11 18:00:51 +0100

    add new line in class of test

--- src/test/java/ighorafi/practice_jenkins/PracticeJenkinsApplicationTests.java
+++ src/test/java/ighorafi/practice_jenkins/PracticeJenkinsApplicationTests.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.http.HttpTimeoutException;
+
 @SpringBootTest
 class PracticeJenkinsApplicationTests {
 
@@ -12,6 +15,9 @@ class PracticeJenkinsApplicationTests {
     @Test
     void contextLoads() {
         logger.info("test case executing...");
+        logger.info("test case executing and this is seconder ");
     }
 
 }
+
+
